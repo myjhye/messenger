4323e0a3700efd3c3ad81812ead4904bea6fe535
myjhye <t01087633799@gmail.com>
2024-06-25 03:06:27 +0900

    change profile setting

---
+++ app/api/settings/route.ts
@@ -0,0 +1,36 @@
+// 프로필 업데이트
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(request: Request) {
+    try {
+        const currentUser = await getCurrentUser();
+        const body = await request.json();
+        const { name, image } = body;
+
+        if (!currentUser?.id) {
+            return new NextResponse('Unauthorized', { status: 401 });
+        }
+
+        // 프로필 업데이트
+        const updatedUser = await prisma.user.update({
+            where: {
+                id: currentUser.id
+            },
+            data: {
+                // 새 이미지 url
+                image: image,
+                name: name,
+            },
+        });
+
+        // 업데이트된 프로필 정보 JSON으로 반환
+        return NextResponse.json(updatedUser);
+
+    } catch (error: any) {
+        console.log(error, 'ERROR_SETTINGS');
+        return new NextResponse('Internal Error', { status: 500 });
+    }
+}
---
+++ app/components/sidebar/SettingsModal.tsx
@@ -0,0 +1,143 @@
+// 프로필 설정 모달 (이름, 프로필 사진)
+
+"use client"
+
+import { User } from "@prisma/client"
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import Modal from "../Modal";
+import Input from "../inputs/Input";
+import Image from "next/image";
+import { CldUploadButton } from "next-cloudinary";
+import Button from "../Button";
+
+interface SettingsModalProps {
+    currentUser: User | null;
+    isOpen?: boolean;
+    onClose: () => void;
+};
+
+export default function SettingsModal({ currentUser, isOpen, onClose }: SettingsModalProps) {
+    
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+
+    // 폼 상태 관리
+    const {
+        // 입력 필드 
+        register,
+        // 폼 제출
+        handleSubmit,
+        // 필드 값 수동 업데이트
+        setValue,
+        // 필드 값 실시간 추적하고 변경 될 때마다 자동 업데이트
+        watch,
+        formState: {
+            errors,
+        },
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: currentUser?.name,
+            image: currentUser?.image,
+        }
+    });
+
+    // 이미지 필드의 현재 값 감시
+    const image = watch('image');
+
+    // 이미지 업로드 핸들러
+    const handleUpload = (result: any) => {
+        setValue('image', result?.info?.secure_url, {
+            shouldValidate: true,
+        })
+    }
+
+    // 폼 제출 핸들러 (useForm으로 폼 데이터를 자동 수집해 간단 처리)
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+        
+        axios.post('/api/settings', data)
+            .then(() => {
+                router.refresh();
+                onClose();
+            })
+            .catch(() => toast.error('Something went wrong!'))
+            .finally(() => setIsLoading(false))
+    };
+    
+    return (
+        <Modal
+            isOpen={isOpen}
+            onClose={onClose}
+        >
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="space-y-12">
+                    <div className="border-b border-gray-900/10 pb-12">
+                        <h2 className="text-base font-semibold leading-7 text-gray-900">
+                            Profile
+                        </h2>
+                        <p className="mt-1 text-sm leading-6 text-gray-600">
+                            Edit your public information
+                        </p>
+                        <div className="mt-10 flex flex-col gap-y-8">
+                            <Input 
+                                disabled={isLoading}
+                                label="Name"
+                                id="name"
+                                errors={errors}
+                                required
+                                // 필드를 폼에 등록
+                                register={register}
+                            />
+                            <div>
+                                <label className="block text-sm font-medium leading-6 text-gray-900">
+                                    Photo
+                                </label>
+                                <div className="mt-2 flex items-center gap-x-3">
+                                    <Image 
+                                        width="48"
+                                        height="48"
+                                        className="rounded-full"
+                                        src={image || currentUser?.image || '/images/placeholder.jpg'}
+                                        alt="Avatar"
+                                    />
+                                    <CldUploadButton
+                                        options={{ maxFiles: 1 }}
+                                        onUpload={handleUpload}
+                                        uploadPreset="mdg81ikp"
+                                    >
+                                        <Button
+                                            disabled={isLoading}
+                                            secondary
+                                            type="button"
+                                        >
+                                            Change
+                                        </Button>
+                                    </CldUploadButton>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-6 flex items-center justify-end gap-x-6">  
+                        <Button
+                            disabled={isLoading}
+                            secondary
+                            onClick={onClose}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            disabled={isLoading}
+                            type="submit"
+                        >
+                            Save
+                        </Button>
+                    </div>
+                </div>
+            </form>    
+        </Modal>
+    )
+}
